206d38638bbe8f1838c5f3b753114180cb91381c
Dawn Kline <dawn.kline.m@gmail.com>
2023-06-08 19:24:22 -0500

    updated code

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -30,6 +31,7 @@ export default function PortfolioContainer() {
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* // TODO: Add a comment explaining what is happening on the following line */}
       {renderPage()}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/pages/Blog.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import svg from "../images/Sample-SquareSVG-generated.png"
+
+export default function Blog() {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus.
+      </p>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <img src={svg} />
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg" />
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
